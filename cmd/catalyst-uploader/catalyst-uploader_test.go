48677f875699b7c7d0a9242a4a32f155a7a808fc
Ivan Poleshchuk <ivan.poleshchuk@outlook.com>
2022-08-11 16:16:49 +0500

    Fix custom S3 implementation and test with min.io

--- cmd/catalyst-uploader/catalyst-uploader_test.go
+++ cmd/catalyst-uploader/catalyst-uploader_test.go
@@ -12,15 +12,67 @@ import (
 	"net/url"
 	"os"
 	"os/exec"
+	"path"
 	"strings"
 	"testing"
 )
+
+func splitNonEmpty(str string, sep rune) []string {
+	splitFn := func(c rune) bool {
+		return c == sep
+	}
+	return strings.FieldsFunc(str, splitFn)
+}
 
 func buildUploader(assert *assert.Assertions) {
 	// build app
 	build := exec.Command("go", strings.Split("build catalyst-uploader.go", " ")...)
 	err := build.Run()
 	assert.NoError(err)
+}
+
+func testE2E(assert *assert.Assertions, fullUriStr string) {
+	buildUploader(assert)
+	// create random data
+	rndData := make([]byte, 1024*128+10)
+	rand.Read(rndData)
+	stdinReader := bytes.NewReader(rndData)
+	// run
+	args := fullUriStr
+	uploader := exec.Command("./catalyst-uploader", strings.Split(args, " ")...)
+	uploader.Stdin = stdinReader
+	stdoutRes, err := uploader.Output()
+	fmt.Println(string(stdoutRes))
+	assert.NoError(err)
+
+	// check output
+	outJson := struct {
+		Uri string `json:"uri"`
+	}{}
+	err = json.Unmarshal(stdoutRes, &outJson)
+	assert.NoError(err)
+
+	// load object and compare contents
+	outUrl, _ := url.Parse(outJson.Uri)
+	fullUri, _ := outUrl.Parse(fullUriStr)
+	bucket := splitNonEmpty(fullUri.Path, '/')[0]
+	if !strings.Contains(outUrl.Host, bucket) {
+		// if bucket is not included in domain name of output URI, then it's already in the path
+		bucket = ""
+	}
+	// compare key after leading slash
+	assert.Equal(fullUri.Path, path.Clean("/"+bucket+"/"+outUrl.Path))
+	os, err := drivers.ParseOSURL(fullUriStr, true)
+	assert.NoError(err)
+	session := os.NewSession("")
+	// second argument is object key and passed to API unmodified
+	data, err := session.ReadData(context.Background(), "")
+	assert.NoError(err)
+	assert.Equal(*data.Size, int64(len(rndData)))
+	osBuf := new(bytes.Buffer)
+	osBuf.ReadFrom(data.Body)
+	osData := osBuf.Bytes()
+	assert.Equal(rndData, osData)
 }
 
 func TestFsHandlerE2E(t *testing.T) {
@@ -62,48 +114,28 @@ func TestFsHandlerE2E(t *testing.T) {
 
 func TestS3HandlerE2E(t *testing.T) {
 	assert := assert.New(t)
-	buildUploader(assert)
-	// create random data
-	rndData := make([]byte, 1024*1024+10)
-	rand.Read(rndData)
-	stdinReader := bytes.NewReader(rndData)
-
-	s3key := os.Getenv("AWS_TEST_KEY")
-	s3secret := os.Getenv("AWS_TEST_SECRET")
-	s3region := os.Getenv("AWS_TEST_REGION")
-	s3bucket := os.Getenv("AWS_TEST_BUCKET")
+	s3key := os.Getenv("AWS_S3_KEY")
+	s3secret := os.Getenv("AWS_S3_SECRET")
+	s3region := os.Getenv("AWS_S3_REGION")
+	s3bucket := os.Getenv("AWS_S3_BUCKET")
 	if s3key != "" && s3secret != "" && s3region != "" && s3bucket != "" {
-		// run
 		testKey := "/test/" + uuid.New().String() + ".ts"
-		args := fmt.Sprintf("s3://%s:%s@%s/%s%s", s3key, s3secret, s3region, s3bucket, testKey)
-		uploader := exec.Command("./catalyst-uploader", strings.Split(args, " ")...)
-		uploader.Stdin = stdinReader
-		stdoutRes, err := uploader.Output()
-		fmt.Println(string(stdoutRes))
-		assert.NoError(err)
+		uri := fmt.Sprintf("s3://%s:%s@%s/%s%s", s3key, s3secret, s3region, s3bucket, testKey)
+		testE2E(assert, uri)
+	} else {
+		fmt.Println("No S3 credentials, test skipped")
+	}
+}
 
-		// check output
-		outJson := struct {
-			Uri string `json:"uri"`
-		}{}
-		err = json.Unmarshal(stdoutRes, &outJson)
-		assert.NoError(err)
-
-		// load object and compare contents
-		url, _ := url.Parse(outJson.Uri)
-		// compare key after leading slash
-		assert.Equal(testKey, url.Path)
-		os, err := drivers.ParseOSURL(fmt.Sprintf("s3://%s:%s@%s/%s%s", s3key, s3secret, s3region, s3bucket, testKey), true)
-		assert.NoError(err)
-		session := os.NewSession("")
-		// second argument is object key and passed to API unmodified
-		data, err := session.ReadData(context.Background(), "")
-		assert.NoError(err)
-		assert.Equal(*data.Size, int64(len(rndData)))
-		osBuf := new(bytes.Buffer)
-		osBuf.ReadFrom(data.Body)
-		osData := osBuf.Bytes()
-		assert.Equal(rndData, osData)
+func TestMinioHandlerE2E(t *testing.T) {
+	assert := assert.New(t)
+	s3key := os.Getenv("MINIO_S3_KEY")
+	s3secret := os.Getenv("MINIO_S3_SECRET")
+	s3bucket := os.Getenv("MINIO_S3_BUCKET")
+	if s3key != "" && s3secret != "" && s3bucket != "" {
+		testKey := "/test/" + uuid.New().String() + ".ts"
+		uri := fmt.Sprintf("s3+http://%s:%s@localhost:9000/%s%s", s3key, s3secret, s3bucket, testKey)
+		testE2E(assert, uri)
 	} else {
 		fmt.Println("No S3 credentials, test skipped")
 	}
