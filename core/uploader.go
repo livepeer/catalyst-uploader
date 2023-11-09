151df895ebba17a09fd8301e290ccc3acf5f1f87
Max Holland <max@livepeer.org>
2023-11-09 17:05:06 +0000

    Increase thumbnail resolution (#43)

--- core/uploader.go
+++ core/uploader.go
@@ -142,7 +142,7 @@ func extractThumb(session drivers.OSSession, filename string, segment []byte) er
 		"-i", inFile,
 		"-ss", "00:00:00",
 		"-vframes", "1",
-		"-vf", "scale=320:240:force_original_aspect_ratio=decrease",
+		"-vf", "scale=854:480:force_original_aspect_ratio=decrease",
 		"-y",
 		outFile,
 	}
