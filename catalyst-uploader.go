8043ee099fd7699444cef0ca6d7bba52b1cc50fc
Max Holland <max@livepeer.org>
2024-01-09 10:18:35 +0000

    Switch success line to glog (#49)
    * Switch success line to glog
    
    Also bump go-tools (reduced logging) and set logtostderr, glog.Info lines didn't show otherwise
    
    * fix

--- catalyst-uploader.go
+++ catalyst-uploader.go
@@ -20,6 +20,10 @@ func main() {
 }
 
 func run() int {
+	err := flag.Set("logtostderr", "true")
+	if err != nil {
+		glog.Fatal(err)
+	}
 	// cmd line args
 	describe := flag.Bool("j", false, "Describe supported storage services in JSON format and exit")
 	timeout := flag.Duration("t", 30*time.Second, "Upload timeout")
@@ -46,17 +50,19 @@ func run() int {
 		return 1
 	}
 
-	err := core.Upload(os.Stdin, uri, WaitBetweenWrites, *timeout)
+	err = core.Upload(os.Stdin, uri, WaitBetweenWrites, *timeout)
 	if err != nil {
 		glog.Fatalf("Uploader failed for %s: %s", uri, err)
 		return 1
 	}
 
 	// success, write uploaded file details to stdout
-	err = json.NewEncoder(stdout).Encode(map[string]string{"uri": uri})
-	if err != nil {
-		glog.Fatal(err)
-		return 1
+	if glog.V(5) {
+		err = json.NewEncoder(stdout).Encode(map[string]string{"uri": uri})
+		if err != nil {
+			glog.Fatal(err)
+			return 1
+		}
 	}
 
 	return 0
